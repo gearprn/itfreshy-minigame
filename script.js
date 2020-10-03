0f17467cc9b27e2589e56f922db027dcfc12b2c0
Icyscools <meet.ice1@gmail.com>
2020-10-03 16:03:12 +0700

    Change shuffle script

--- script.js
+++ script.js
@@ -32,23 +32,45 @@ const shuffle = () => {
     "เสียเงินรางวัล",
   ];
 
+  prize = arr_shuffle(prize);
+
   let randomPrize;
   // random prize into back of card
-  resultList.map((record) => {
-    const index = Math.floor(random(1, prize.length)) - 1;
+  resultList.map((record, index) => {
+    // const index = Math.floor(random(1, prize.length)) - 1;
     const text = prize[index];
     randomPrize = text;
     const result = document.getElementById(record);
     result.innerHTML = text;
   });
+
   prize.filter((rec) => {
-    console.log(rec);
     return rec != randomPrize;
   });
 };
 const random = (mn, mx) => {
   return Math.random() * (mx - mn) + mn;
 };
+
+function arr_shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 const handleClick = (cardInnerId) => {
   const cardInnder = document.getElementById(cardInnerId);
@@ -71,7 +93,6 @@ const resetAll = () => {
 };
 
 const openAll = () => {
-  console.log("c");
   cardName.map((card) => {
     const cardInner = document.getElementById(card);
     console.log(cardInner.classList);
